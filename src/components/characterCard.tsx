a3541f840705ca1311607ccd8f61d08ead7bd21a
Florent Giov annone <giovannoneflorent@gmail.com>
2024-03-08 10:44:21 +0000

    Finnished card styling

--- src/components/characterCard.tsx
+++ src/components/characterCard.tsx
@@ -20,7 +20,7 @@ function CharacterCard({
     <>
       <section>
         <div style={{ height: 250, width: 200 }} className="card  m-1">
-          <div className="card-header-title center-text">{fullName}</div>
+          <div className="card-header-title center-text has-text-light has-background-dark">{fullName}</div>
           <div className="card-imagep1 p-2">
             <Link to={`/characters/${id}`}>
               <figure className="image image is-square">
